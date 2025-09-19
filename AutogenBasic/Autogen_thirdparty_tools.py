e8576c62c11b35eb08863f00956911cd878cd97b
vmndilip-star <vmndilip@gmail.com>
2025-09-19 04:20:51 -0500

    Selectors

--- AutogenBasic/Autogen_thirdparty_tools.py
+++ AutogenBasic/Autogen_thirdparty_tools.py
@@ -1,15 +1,61 @@
-
 import asyncio
-from autogen.agentchat import AssistantAgent
+from autogen_agentchat.agents import AssistantAgent
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 from autogen_core.tools import FunctionTool
-from autogen_ext.tools.http import HttpTool
 import os
+from autogen_core.tools import FunctionTool
 from dotenv import load_dotenv
-from langchain_community.utilities import GoogleSearchAPIWrapper
+
+from langchain_community.utilities import GoogleSerperAPIWrapper
+
 from autogen_ext.tools.http import HttpTool
+
+# Load environment variables
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 if not api_key:
-    raise ValueError("OPENAI_API_KEY environment variable not set")
-model_client = OpenAIChatCompletionClient(model='gpt-4o', api_key=api_key)
+    raise ValueError("Please set the OPENAI_API_KEY environment variable.")
+
+model_client=OpenAIChatCompletionClient(model='gpt-4o',api_key=api_key)
+
+Serper=os.environ['SERPER_API_KEY']='SERPER_API_KEY_HERE'  # Replace with your actual Serper API key
+
+
+search_tool_wrapper = GoogleSerperAPIWrapper(type='search')
+
+def search_web(query:str) ->str:
+    """Search the web for the given query and return the results."""
+
+    if (query == 'ipl'):
+        return 'IPL is Indian Premier League, a professional Twenty20 cricket league in India.' # Mocking the call
+    try:
+        results = search_tool_wrapper.run(query)
+        return results
+    except Exception as e:
+        print(f"Error occurred while searching the web: {e}")
+        return "No results found."  
+
+
+search_agent = AssistantAgent(
+    name="SearchAgent",
+    model_client=model_client,
+    tools=[search_web],
+    description="An agent that can search the web for information.",
+    system_message="You are a helpful assistant that can search the web for information using the search_web tool." \
+    "Please make sure that you use the search_web tool to find information before you return the answer." \
+    "don't send the year in query, rather use latest or recently etc.",
+    reflect_on_tool_use=True,
+)
+
+async def run_serper_search():
+    """Run the search agent with a sample query."""
+    query = "Who won the IPL recently ?" 
+    print(f"Querying: {query}")
+    
+    
+    result = await search_agent.run(task=query)
+    print(result.messages[-1].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(run_serper_search())
